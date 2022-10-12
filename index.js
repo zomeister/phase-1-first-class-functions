9c0851f1a030ca3ecfac9c9693ed8c93b0754175
Zoe Guidry <oyezoey@gmail.com>
2022-10-12 06:17:00 -0500

    commit tree 9701a322c2030d422931c821868651a3b7637493

---
+++ index.js
@@ -0,0 +1,13 @@
+function receivesAFunction(callback) {
+    callback();
+  }
+  
+  function returnsANamedFunction() {
+    return function namedFn() {
+      console.log("this is a named function");
+    };
+  }
+  
+  function returnsAnAnonymousFunction() {
+    return () => console.log("this is an anonymous function");
+  }
